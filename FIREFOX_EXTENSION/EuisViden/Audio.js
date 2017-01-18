a1ccd93882a910c9857b9eb0843bf19430427cef
MaximeJack <maxime_jacquet@orange.fr>
2017-01-18 11:00:46 +0100

    ajout click for relecture

--- FIREFOX_EXTENSION/EuisViden/Audio.js
+++ FIREFOX_EXTENSION/EuisViden/Audio.js
@@ -25,6 +25,7 @@ for (var i = 0; i < elemts.length; i++) {
     if (elemts[i].nodeName != "SCRIPT" && elemts[i].nodeName != "#text") {
         console.log(elemts[i]);
         texts += elemts[i].textContent;
+        elemts[i].onclick=speak(elemts[i].textContent);
     }
 }
 console.log(texts);
