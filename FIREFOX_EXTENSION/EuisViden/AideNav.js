47e24af8e5a579154352f1ac2dafe708fee68acd
B3lpheg0r <dieuset.romain@gmail.com>
2017-01-19 09:05:32 +0100

    aide nav google recheche

--- FIREFOX_EXTENSION/EuisViden/AideNav.js
+++ FIREFOX_EXTENSION/EuisViden/AideNav.js
@@ -1,31 +1,99 @@
 /**
  * Created by maxim on 17/01/2017.
- */
+ 
+*/
+
+AideNav();
+raccourci();
+
+var IndexSelectionSite = -1;
+var tabResultRechecheNom = new Array();
+var tabResultRechecheURL = new Array();
 
 
-//[type=text]
-	var x = document.body.getElementsByTagName('input');
-	for (var i = 0; i < x.length; i++){
-		x[i].textRecherche = "";
-		if(x[i].getAttribute('type')=='text' || !x[i].hasAttribute('type'))
-		{	
-			//pour ecrire
-			x[i].onkeypress=function(e){
-				console.log(e);
-				if (e.keyCode == 0){//charactere
-					x[i].textRecherche+=e.key;
-				}
-				if (e.keyCode == 8 && x[i].textRecherche.length > 0){//backspace
-						x[i].textRecherche = x[i].textRecherche.substr(0,x[i].textRecherche.length-1);
-				}
-				// if(e.keyCode == 35)
-				// 	speak(x[i].textRecherche);
-			};
+var IsGoogle =window.location.href.indexOf("://www.google.fr/")>-1;
+var IsFisrtPage = (window.location.href.indexOf("#q=") < 0) && (window.location.href.indexOf("?q=") < 0);
 
-			
+console.log(IsGoogle + " : " +IsFisrtPage );
 
+if (IsGoogle && IsFisrtPage) {
+	AffichePrompt();
+}
+if (IsGoogle && !IsFisrtPage) {
+	speak('Pour lancer une nouvelle recherche appuyer sur F2');
+	GetRes();
+}
+
+function AffichePrompt(){
+	speak("Recherche google !");
+	var rech = prompt('Recherche sur Google ?');
+	console.log(rech);
+	if(rech != "" && rech != null){
+		speak("Votre recherche est : " + rech);
+		speak("Entrer pour valider ou echap pour quitter");
+		var verif = confirm('Confirmer ?');
+
+		if(verif){
+			window.location.href = 'https://www.google.fr/search?q='+rech;
+		}
+	}else{
+		speak("Modifier votre recherche ? Appuyer sur Entrer pour valider ou echap pour quitter")
+		var verif = confirm('Confirmer ?');
+		if(verif){
+			AffichePrompt();		
 		}
 	}
+}
+
+
+function raccourci() {
+	document.onkeypress = function(e){
+		console.log(e);
+		//Retour page suivante
+		if (e.keyCode == 33) {/*PageUp*/ javascript:history.forward();}
+		//Retour page precedente
+		if (e.keyCode == 34) {/*PageDown*/ javascript:history.back();}
+		//Affiche Prompt 
+		if (e.keyCode == 113) {/*F2*/ AffichePrompt();}
+		//Selection site suivant
+		if (e.keyCode == 113) {/*fleche du bas*/ LireNomSite();}
+		//Selection site precedant
+		if (e.keyCode == 113) {/*fleche du bas*/ LireNomSite();}
+		//Valide Selection Site
+		if (e.keyCode == 113) {/*F2*/ ValideSite();}
+	}
+}
+
+function LireNomSite(argument) {
+	if (IndexSelectionSite > -1) {
+		speak("Site : " + tabResultRechecheNom[IndexSelectionSite]);
+	}
+}
+
+function ValideSite(argument){
+	if (IndexSelectionSite > -1) {
+		speak("Aller sur  : " + tabResultRechecheNom[IndexSelectionSite]);
+		speak("Entrer pour valider ou echap pour quitter");
+		window.location.href = tabResultRechecheURL[IndexSelectionSite]);
+	}
+}
+
+
+function GetRes(argument){
+	//var res = document.body.getElementsByName('cite');
+	var res = document.body.getElementsByClassName('r');
+
+	console.log(res.length);
+	for (var i = 0; i < res.length; i++){
+		tabResultRechecheNom[i] = res[i].textContent;
+		tabResultRechecheURL[i] = res[i].firstChild.getAttribute('href');
+	}
+
+
+	IndexSelectionSite = 0;
+	console.log(tabResultRechecheURL);
+}
+
 
 function talk(obj){
     speak(obj);
