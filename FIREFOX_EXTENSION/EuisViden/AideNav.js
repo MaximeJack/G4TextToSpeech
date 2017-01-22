a657795be574edfbd168f86497989ef54320d3ef
BarataMathieu <mathieu@barata.fr>
2017-01-22 14:25:16 +0100

    debug du aide nav

--- FIREFOX_EXTENSION/EuisViden/AideNav.js
+++ FIREFOX_EXTENSION/EuisViden/AideNav.js
@@ -12,7 +12,7 @@ var tabResultRechecheURL = new Array();
 var IsGoogle =window.location.href.indexOf("://www.google.")>-1;
 var IsFisrtPage = (window.location.href.indexOf("#q=") < 0) && (window.location.href.indexOf("?q=") < 0);
 
-raccourci();
+
 
 console.log(IsGoogle + " : " +IsFisrtPage );
 
@@ -26,43 +26,7 @@ if (IsGoogle && !IsFisrtPage) {
 	GetRes();
 }
 
-function raccourci() {
 
-	document.onkeypress = function(e){
-		// e.preventDefault();
-		console.log(e);
-		//Retour page suivante
-		if (navigator.userAgent.indexOf('OPR')>-1) {//opera
-			console.log('opera');
-
-		}else if(navigator.userAgent.indexOf('Safari')>-1){//chrome
-			console.log('chrome');
-		}else{//mozilla
-			console.log('firefox');
-			if (e.keyCode == 33) {/*PageUp*/ javascript:history.forward();}
-			//Retour page precedente
-			if (e.keyCode == 34) {/*PageDown*/ javascript:history.back();}
-			//Affiche Prompt 
-			if (e.keyCode == 113) {/*F2*/ AffichePrompt();}
-
-			//Selection site suivant
-			if(e.keyCode == 38){/*fleche haut*/
-				console.log('suiv');
-				SelectionSite(1); 
-				LireNomSite();
-				e.preventDefault();
-			}
-			//Selection site precedant
-			if(e.keyCode == 40){console.log('prec');/*fleche du bas*/SelectionSite(-1); LireNomSite();e.preventDefault();}
-
-				//Valide Selection Site
-			if (e.keyCode == 39) {/*fleche de droite*/ValideSite();}
-			//lire la selection
-			if (e.keyCode == 37) {/*fleche de gauche*/ LireNomSite();}
-			
-		}
-	}
-}
 
 function AffichePrompt(){
 	StopSpeak();
--- FIREFOX_EXTENSION/EuisViden/AideNav/AideNavContent.js
+++ FIREFOX_EXTENSION/EuisViden/AideNav/AideNavContent.js
@@ -2,7 +2,7 @@ var VARIABLE_GLOBALE_AIDE_NAV = false;
 
 function setVARIABLE_GLOBALE_AIDE_NAV(i){
    if(typeof i !== 'undefined'){
-      VARIABLE_GLOBALE_AUDIO = i;  
+      VARIABLE_GLOBALE_AIDE_NAV = i;  
    }
   
 }
@@ -16,6 +16,45 @@ function envoieAideNav(){
 
 
 
+
+
+	document.onkeypress = function(e){
+            if(VARIABLE_GLOBALE_AIDE_NAV){
+                console.log('xxx');
+		// e.preventDefault();
+		console.log(e);
+		//Retour page suivante
+		if (navigator.userAgent.indexOf('OPR')>-1) {//opera
+			console.log('opera');
+
+		}else if(navigator.userAgent.indexOf('Safari')>-1){//chrome
+			console.log('chrome');
+		}else{//mozilla
+			console.log('firefox');
+			if (e.keyCode == 33) {/*PageUp*/ javascript:history.forward();}
+			//Retour page precedente
+			if (e.keyCode == 34) {/*PageDown*/ javascript:history.back();}
+			//Affiche Prompt 
+			if (e.keyCode == 113) {/*F2*/ AffichePrompt();}
+
+			//Selection site suivant
+			if(e.keyCode == 38){/*fleche haut*/
+				console.log('suiv');
+				SelectionSite(1); 
+				LireNomSite();
+				e.preventDefault();
+			}
+			//Selection site precedant
+			if(e.keyCode == 40){console.log('prec');/*fleche du bas*/SelectionSite(-1); LireNomSite();e.preventDefault();}
+
+				//Valide Selection Site
+			if (e.keyCode == 39) {/*fleche de droite*/ValideSite();}
+			//lire la selection
+			if (e.keyCode == 37) {/*fleche de gauche*/ LireNomSite();}
+			
+		}
+            }
+	}
 
 
 envoieAideNav();
